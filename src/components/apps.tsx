d09baa39b8efbdd5c0e5bf7519766ae9363174a6
Alexis Joel Leite Paracampo <alexisleite@live.com>
2021-09-12 12:04:58 -0300

    Delete apps.tsx

--- src/components/apps.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface IAppsProps {}
-
-export default class Apps extends React.Component<IAppsProps> {
-  public render() {
-    return <div id="">A ver qué pasa si meto esto aca</div>;
-  }
-}
